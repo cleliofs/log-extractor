23112f41de441efcaecf5af5b410ef0b47befd58
Clelio De Souza <cleliofs@gmail.com>
2015-06-09 21:09:27 +0100

    Refactored Java version to be more functional

--- src/main/java/uk/sky/DataFilterer.java
+++ src/main/java/uk/sky/DataFilterer.java
@@ -3,7 +3,10 @@ package uk.sky;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Spliterators;
 import java.util.function.Consumer;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
@@ -11,59 +14,40 @@ import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
 
 import static java.lang.String.format;
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class DataFilterer {
     private static final Pattern logExtractRegex = Pattern.compile("([\\d]+),([A-Z]+),([\\d]+)");
 
     public static Collection<?> filterByCountry(Reader source, String country) {
-        final List<String> result = new ArrayList<>();
         final List<String> lines = getLinesFromReader(source);
-        for (String line : lines) {
-            final Set<String> countriesSet = StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(line)), false)
-                    .filter(matchResult -> matchResult.group(2).equalsIgnoreCase(country))
-                    .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
-                    .collect(toSet());
-            result.addAll(countriesSet);
-        }
-
-        return result;
+        return lines.stream().skip(1).map(l -> StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(l)), false)
+                .filter(matchResult -> matchResult.group(2).equalsIgnoreCase(country))
+                .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
+                .collect(joining())).filter(s -> !s.isEmpty()).collect(toList());
     }
 
     public static Collection<?> filterByCountryWithResponseTimeAboveLimit(Reader source, String country, long limit) {
-        final List<String> result = new ArrayList<>();
         final List<String> lines = getLinesFromReader(source);
-        for (String line : lines) {
-            final Set<String> countriesSet = StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(line)), false)
-                        .filter(matchResult -> matchResult.group(2).equalsIgnoreCase(country))
-                        .filter(matchResult -> new Long(matchResult.group(3)) > limit)
-                        .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
-                        .collect(toSet());
-            result.addAll(countriesSet);
-        }
-
-        return result;
+        return lines.stream().skip(1).map(l -> StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(l)), false)
+                .filter(matchResult -> matchResult.group(2).equalsIgnoreCase(country))
+                .filter(matchResult -> new Long(matchResult.group(3)) > limit)
+                .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
+                .collect(joining()))
+                .filter(s -> !s.isEmpty()).collect(toList());
     }
 
     public static Collection<?> filterByResponseTimeAboveAverage(Reader source) {
-        final List<String> result = new ArrayList<>();
         final List<String> lines = getLinesFromReader(source);
-        long sum = 0;
-        for (String line : lines) {
-            sum += StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(line)), false)
-                    .mapToLong(matchResult -> new Long(matchResult.group(3)))
-                    .sum();
-        }
-        final long average = sum / (lines.size() - 1);
-        for (String line : lines) {
-            final Set<String> aboveAverageRequests = StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(line)), false)
-                    .filter(matchResult -> new Long(matchResult.group(3)) > average)
-                    .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
-                    .collect(toSet());
-            result.addAll(aboveAverageRequests);
-        }
-
-        return result;
+        final double average = lines.stream().skip(1).map(l -> StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(l)), false)
+                .mapToLong(matchResult -> new Long(matchResult.group(3)))
+                .sum()).mapToLong(l -> l).average().getAsDouble();
+        return lines.stream().skip(1).map(l -> StreamSupport.stream(new MatcherSpliterator(logExtractRegex.matcher(l)), false)
+                .filter(matchResult -> new Long(matchResult.group(3)) > average)
+                .map(matchResult -> format("%s,%s,%s", matchResult.group(1), matchResult.group(2), matchResult.group(3)))
+                .collect(joining()))
+                .filter(s -> !s.isEmpty()).collect(toList());
     }
 
 
